7e3699f022cd6c36894d116a3802f6af2e4e2393
francisyzy <github@francisyzy.com>
2021-02-15 01:35:34 +0800

    Remove child foldering in python script

--- files.py
+++ files.py
@@ -4,29 +4,25 @@ from pathlib import Path
 from pprint import pprint
 
 p = Path("static/sounds/")
-sounds = {"Captain Holt": [], "Terry Jeffords": [], "Charles Boyle": [], "Jake Peralta": [], "Gina Linetti": [], "Rosa Diaz": [], "Amy Santiago": [], "Hitchcock": [], "Scully": [], "Captain CJ": [], "Doug Judy": [], "Kevin": []}
-characters = list(sounds.keys())
+sounds = []
 
 for i in p.glob('**/*.mp3'):
-  sound_path = str(i).split("sounds/")[1]
+  sound_path = str(i).split("sounds")[1]
 
   # make sure not system file
   if not (sound_path[0] == "."):
     
-    character, filename = sound_path.split('/')[0], sound_path.split('/')[1]
+    filename = sound_path.split('/')[0]
 
-    try: sounds[character].append(filename)
-    except: sounds[character] = [filename]
+    try: sounds.append(filename)
+    except: sounds = [filename]
 
-for char, list in sounds.items():
-  sounds[char].sort()
 
 # pprint(sounds)
 
 # write to ../contents/sounds.js
 file = open('contents/sounds.js', 'w')
 file.write(f"export let sounds = {str(sounds)};\n" )
-file.write(f"export let characters = {str(characters)};")
 file.close()
 
 print("Done.")
